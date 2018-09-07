a366c4da25ace281e3fbe3258128fe7138dfc8d8
Ivan Grimes <ivan@grimesco.de>
2018-09-07 12:26:31 +0300

    remove store from window, remove logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import createHistory from 'history/createBrowserHistory';
 import { routerMiddleware } from 'react-router-redux';
@@ -12,15 +11,12 @@ const saga = createSagaMiddleware();
 const middleware = applyMiddleware(
   routerMiddleware(history),
   saga,
-  logger,
 );
 
 const store = createStore(
   rootReducer,
   middleware,
 );
-
-window.store = store; // TODO: Delete when you finished
 
 saga.run(rootSaga);
 export default store;
